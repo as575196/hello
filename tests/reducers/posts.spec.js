0ce295983519a71edf93a725ca4cf3757e0d6d29
as5751962 <73004268+as5751962@users.noreply.github.com>
2020-10-17 05:23:34 +0530

    Update posts.spec.js

--- tests/reducers/posts.spec.js
+++ tests/reducers/posts.spec.js
@@ -3,7 +3,6 @@ import posts from '../../client/reducers/posts';
 import defaultPosts from '../../client/data/posts';
 
 describe('Posts Reducer', function () {
-  
   it('should add a like to an existing post', function () {
     const index = 2;
     const action = { type: 'INCREMENT_LIKES', index };
