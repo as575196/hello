6f8dd073f61164a98528c035a00108be22a462b6
Pritika <73077515+pritikazmailbox@users.noreply.github.com>
2020-10-19 13:09:34 +0530

    Update posts.js

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,7 @@ function posts(state = [], action) {
     case 'INCREMENT_LIKES' :
       const i = action.index;
       return [
-        ...state.slice(0, i),
-        {...state[i],  likes: state[i].likes + 1 },
-        ...state.slice(i + 1)
+        ...state.slice(0, i), {...state[i],  likes: state[i].likes + 1 }, ...state.slice(i + 1)
       ];
     default:
       return state;
